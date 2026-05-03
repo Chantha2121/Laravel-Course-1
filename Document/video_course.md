45155ec70bd929c61c9322df15f743fb57bc3825
choeurn chantha <choeurnchantha168@gmail.com>
2026-05-03 14:59:03 +0700

    update

--- Document/video_course.md
+++ Document/video_course.md
@@ -1,15 +1,25 @@
 # Video Review Course
+
 ### Part 1: Introduction to Course
+
 Link : https://youtu.be/yhguOGA6JGE
 
 ### Part 2: Git Course Part 1
+
 Link : https://youtu.be/JbmRJgLr-VE
 
 ### Part 3: Git Course Part 2
+
 Link : https://youtu.be/P7jX6vbvJJA
 
 ### Part 4: Git Course Part 3
+
 Link : https://youtu.be/1ntH0c6GHTg
 
 ### Part 5: Git Course Part 4
+
 Link : https://youtu.be/STzrWZIc-W0
+
+### Part 6: PHP Course Part 1
+
+Link : https://youtu.be/b_POVYfetAM
---
+++ pratice_code/php/exercise4/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Simple html statement
+  echo "One line simple string <br/>";
+
+  // Display string within paragraph different color
+
+  echo "<p><font color=blue>one line simple string is blue</font></p>";
+  echo "<p><font color=red>one line simple string is red</font></p>";
+  echo "<p><font color=green>one line simple string is green</font></p>";
+  ?>
+</body>
+
+</html>
